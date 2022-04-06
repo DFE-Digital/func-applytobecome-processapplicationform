c6c068d163552d1d4cd4eebcb3991f29f5efd7d9
Catherine LAWLOR <Cath.LAWLOR@gmail.com>
2022-04-06 14:54:04 +0100

    set expected model for test

--- ProcessApplicationForm.Test/MapperTests/ApplyingSchoolMapperTests.cs
+++ ProcessApplicationForm.Test/MapperTests/ApplyingSchoolMapperTests.cs
@@ -1,8 +1,10 @@
+using System;
 using System.Collections.Generic;
-using AutoFixture;
+using FizzWare.NBuilder;
 using FluentAssertions;
 using Moq;
 using ProcessApplicationFormFunction.Database.Models;
+using ProcessApplicationFormFunction.Extensions;
 using ProcessApplicationFormFunction.Mappers;
 using Xunit;
 
@@ -23,7 +25,6 @@ public class ApplyingSchoolMapperTests
         _mockSchoolLease
             .Setup(x => x.Map(It.IsAny<ICollection<StagingSchoolLease>>()))
             .Returns(new List<A2BSchoolLease>());
-
     }
 
     [Fact]
@@ -40,52 +41,147 @@ public class ApplyingSchoolMapperTests
     [Fact]
     public void Map_WhenGivenCollectionOfStagingApplicationApplyingSchool_ShouldReturnMappedCollectionOfA2BApplicationApplyingSchool()
     {
-        Fixture fixture = new();
-        fixture.Customize<StagingApplyingSchool>(custom =>
-        {
-            custom.With(p => p.SchoolDeclarationBodyAgree, 907660000);
-            custom.With(p => p.SchoolDeclarationTeacherChair, 907660000);
-            custom.With(p => p.SchoolConversionTargetDateDifferent, 907660000);
-            custom.With(p => p.SchoolConversionChangeName, 907660000);
-            custom.With(p => p.SchoolAdInspectedButReportNotPublished, 907660000);
-            custom.With(p => p.SchoolLaReorganization, 907660000);
-            custom.With(p => p.SchoolLaClosurePlans, 907660000);
-            custom.With(p => p.SchoolPartOfFederation, 907660000);
-            custom.With(p => p.SchoolAddFurtherInformation, 907660000);
-            custom.With(p => p.SchoolAdSafeguarding, 907660000);
-            custom.With(p => p.SchoolSACREExemption, 907660000);
-            custom.With(p => p.SchoolSupportedFoundation, 907660000);
-            custom.With(p => p.SchoolAdEqualitiesImpactAssessment, 907660000);
-            custom.With(p => p.SchoolFinancialInvestigations, 907660000);
-            custom.With(p => p.SchoolFinancialInvestigationsTrustAware, 907660000);
-            custom.With(p => p.SchoolBuildLandSharedFacilities, 907660000);
-            custom.With(p => p.SchoolBuildLandWorksPlanned, 907660000);
-            custom.With(p => p.SchoolBuildLandGrants, 907660000);
-            custom.With(p => p.SchoolBuildLandPriorityBuildingProgramme, 907660000);
-            custom.With(p => p.SchoolBuildLandFutureProgramme, 907660000);
-            custom.With(p => p.SchoolBuildLandPFIScheme, 907660000);
-            custom.With(p => p.SchoolConsultationStakeholders, 907660000);
-            custom.With(p => p.SchoolBuildLandPFIScheme, 907660000);
-            custom.With(p => p.SchoolPFYCapitalSurplusOrDeficit, 907660000);
-            custom.With(p => p.SchoolPFYRevenueSurplusOrDeficit, 907660000);
-            custom.With(p => p.SchoolCFYCapitalSurplusOrDeficit, 907660000);
-            custom.With(p => p.SchoolCFYRevenueSurplusOrDeficit, 907660000);
-            custom.With(p => p.SchoolNFYCapitalSurplusOrDeficit, 907660000);
-            custom.With(p => p.SchoolNFYRevenueSurplusOrDeficit, 907660000);
-            custom.With(p => p.SchoolSupportGrantFundsPaidTo, 907660000);
+        Random rnd = new Random();
 
-            return custom;
-        });
+        var stagingApplyingSchool = Builder<StagingApplyingSchool>
+            .CreateNew()
+            .With(p => p.SchoolDeclarationBodyAgree, 907660000)
+            .With(p => p.SchoolDeclarationTeacherChair, 907660000)
+            .With(p => p.SchoolConversionTargetDateDifferent, 907660000)
+            .With(p => p.SchoolConversionChangeName, 907660000)
+            .With(p => p.SchoolAdInspectedButReportNotPublished, 90766000)
+            .With(p => p.SchoolLaReorganization, 907660000)
+            .With(p => p.SchoolLaClosurePlans, 907660000)
+            .With(p => p.SchoolPartOfFederation, 907660000)
+            .With(p => p.SchoolAddFurtherInformation, 907660000)
+            .With(p => p.SchoolAdSafeguarding, 907660000)
+            .With(p => p.SchoolSACREExemption, 907660000)
+            .With(p => p.SchoolSupportedFoundation, 907660000)
+            .With(p => p.SchoolAdEqualitiesImpactAssessment, 907660000)
+            .With(p => p.SchoolFinancialInvestigations, 907660000)
+            .With(p => p.SchoolFinancialInvestigationsTrustAware, 907660000)
+            .With(p => p.SchoolBuildLandSharedFacilities, 907660000)
+            .With(p => p.SchoolBuildLandWorksPlanned, 907660000)
+            .With(p => p.SchoolBuildLandGrants, 907660000)
+            .With(p => p.SchoolBuildLandPriorityBuildingProgramme, 907660000)
+            .With(p => p.SchoolBuildLandFutureProgramme, 907660000)
+            .With(p => p.SchoolBuildLandPFIScheme, 907660000)
+            .With(p => p.SchoolConsultationStakeholders, 907660000)
+            .With(p => p.SchoolBuildLandPFIScheme, 907660000)
+            .With(p => p.SchoolPFYCapitalSurplusOrDeficit, 907660000)
+            .With(p => p.SchoolPFYRevenueSurplusOrDeficit, 907660000)
+            .With(p => p.SchoolCFYCapitalSurplusOrDeficit, 907660000)
+            .With(p => p.SchoolCFYRevenueSurplusOrDeficit, 907660000)
+            .With(p => p.SchoolNFYCapitalSurplusOrDeficit, 907660000)
+            .With(p => p.SchoolNFYRevenueSurplusOrDeficit, 907660000)
+            .With(p => p.SchoolSupportGrantFundsPaidTo, 907660000)
+            .With(p => p.SchoolCapacityPublishedAdmissionsNumber, rnd.Next().ToString())
+            .Build();
 
-        var stagingApplicationApplyingSchool = fixture.Create<StagingApplyingSchool>();
-           
-        List<StagingApplyingSchool> applyingSchool = new() { stagingApplicationApplyingSchool };
+        List<StagingApplyingSchool> applyingSchool = new() { stagingApplyingSchool };
         List<A2BApplicationApplyingSchool> expectedResult = new()
         {
-            //new()
-            //{
-            //    SchoolDeclarationBodyAgree
-            //}
+            // document links missing from staging model - remove from A2BApplicationApplyingSchool model?
+            new()
+            {
+                Name = stagingApplyingSchool.Name,
+                SchoolAddFurtherInformation = stagingApplyingSchool.SchoolAddFurtherInformation.ConvertDynamicsIntBool(),
+                SchoolAdEqualitiesImpactAssessment = stagingApplyingSchool.SchoolAdEqualitiesImpactAssessment.ConvertDynamicsIntBool(),
+                SchoolAdEqualitiesImpactAssessmentDetails = stagingApplyingSchool.SchoolAdEqualitiesImpactAssessmentDetails,
+                SchoolAdFeederSchools = stagingApplyingSchool.SchoolAdFeederSchools,
+                SchoolAdInspectedButReportNotPublished = stagingApplyingSchool.SchoolAdInspectedButReportNotPublished.ConvertDynamicsIntBool(),
+                SchoolAdInspectedReportNotPublishedExplain = stagingApplyingSchool.SchoolAdInspectedReportNotPublishedExplain,
+                SchoolAdSafeguarding = stagingApplyingSchool.SchoolAdSafeguarding.ConvertDynamicsIntBool(),
+                SchoolAdSafeguardingExplained = stagingApplyingSchool.SchoolAdSafeguardingExplained,
+                SchoolAdSchoolContributionToTrust = stagingApplyingSchool.SchoolAdSchoolContributionToTrust,
+                SchoolBuildLandFutureProgramme = stagingApplyingSchool.SchoolBuildLandFutureProgramme.ConvertDynamicsIntBool(),
+                SchoolBuildLandGrants = stagingApplyingSchool.SchoolBuildLandGrants.ConvertDynamicsIntBool(),
+                SchoolBuildLandGrantsBody = stagingApplyingSchool.SchoolBuildLandGrantsBody,
+                SchoolBuildLandOwnerExplained = stagingApplyingSchool.SchoolBuildLandOwnerExplained,
+                SchoolBuildLandPFIScheme = stagingApplyingSchool.SchoolBuildLandPFIScheme.ConvertDynamicsIntBool(),
+                SchoolBuildLandPFISchemeType = stagingApplyingSchool.SchoolBuildLandPFISchemeType,
+                SchoolBuildLandPriorityBuildingProgramme = stagingApplyingSchool.SchoolBuildLandPriorityBuildingProgramme.ConvertDynamicsIntBool(),
+                SchoolBuildLandSharedFacilities = stagingApplyingSchool.SchoolBuildLandSharedFacilities.ConvertDynamicsIntBool(),
+                SchoolBuildLandSharedFacilitiesExplained = stagingApplyingSchool.SchoolBuildLandSharedFacilitiesExplained,
+                SchoolBuildLandWorksPlanned = stagingApplyingSchool.SchoolBuildLandWorksPlanned.ConvertDynamicsIntBool(),
+                SchoolBuildLandWorksPlannedDate = stagingApplyingSchool.SchoolBuildLandWorksPlannedDate,
+                SchoolBuildLandWorksPlannedExplained = stagingApplyingSchool.SchoolBuildLandWorksPlannedExplained,
+                SchoolCapacityAssumptions = stagingApplyingSchool.SchoolCapacityAssumptions,
+                SchoolCapacityPublishedAdmissionsNumber = int.Parse(stagingApplyingSchool.SchoolCapacityPublishedAdmissionsNumber), // TODO
+                SchoolCapacityYear1 = stagingApplyingSchool.SchoolCapacityYear1,
+                SchoolCapacityYear2 = stagingApplyingSchool.SchoolCapacityYear2,
+                SchoolCapacityYear3 = stagingApplyingSchool.SchoolCapacityYear3,
+                SchoolConsultationStakeholders = stagingApplyingSchool.SchoolConsultationStakeholders.ConvertDynamicsIntBool(),
+                SchoolConsultationStakeholdersConsult = stagingApplyingSchool.SchoolConsultationStakeholdersConsult,
+                SchoolConversionApproverContactEmail = stagingApplyingSchool.SchoolConversionApproverContactEmail,
+                SchoolConversionApproverContactName = stagingApplyingSchool.SchoolConversionApproverContactName,
+                SchoolConversionChangeName = stagingApplyingSchool.SchoolConversionChangeName.ConvertDynamicsIntBool(),
+                SchoolConversionChangeNameValue = stagingApplyingSchool.SchoolConversionChangeNameValue,
+                SchoolConversionContactChairEmail = stagingApplyingSchool.SchoolConversionContactChairEmail,
+                SchoolConversionContactChairName = stagingApplyingSchool.SchoolConversionContactChairName,
+                SchoolConversionContactChairTel = stagingApplyingSchool.SchoolConversionContactChairTel,
+                SchoolConversionContactHeadEmail = stagingApplyingSchool.SchoolConversionContactChairEmail,
+                SchoolConversionContactHeadName = stagingApplyingSchool.SchoolConversionContactHeadName,
+                SchoolConversionContactHeadTel = stagingApplyingSchool.SchoolConversionContactChairTel,
+                SchoolConversionContactRole = stagingApplyingSchool.SchoolConversionMainContactOther.ConvertApplicationRole(),
+                SchoolConversionMainContactOtherRole = stagingApplyingSchool.SchoolConversionMainContactOtherRole,
+                SchoolConversionMainContactOtherEmail = stagingApplyingSchool.SchoolConversionMainContactOtherEmail,
+                SchoolConversionMainContactOtherName = stagingApplyingSchool.SchoolConversionMainContactOtherName,
+                SchoolConversionMainContactOtherTelephone = stagingApplyingSchool.SchoolConversionMainContactOtherTelephone,
+                SchoolConversionReasonsForJoining = stagingApplyingSchool.SchoolConversionReasonsForJoining,
+                SchoolConversionTargetDateDifferent = stagingApplyingSchool.SchoolConversionTargetDateDifferent.ConvertDynamicsIntBool(),
+                SchoolConversionTargetDateDate = stagingApplyingSchool.SchoolConversionTargetDateDate,
+                SchoolConversionTargetDateExplained = stagingApplyingSchool.SchoolConversionTargetDateExplained,
+                SchoolDeclarationBodyAgree = stagingApplyingSchool.SchoolDeclarationBodyAgree.ConvertDynamicsIntBool(),
+                SchoolDeclarationSignedByEmail = stagingApplyingSchool.SchoolDeclarationSignedByEmail,
+                SchoolDeclarationSignedById = stagingApplyingSchool.SchoolDeclarationSignedById,
+                SchoolDeclarationSignedByName = stagingApplyingSchool.SchoolDeclarationSignedByName,
+                SchoolDeclarationTeacherChair = stagingApplyingSchool.SchoolDeclarationTeacherChair.ConvertDynamicsIntBool(),
+                SchoolFaithSchool = stagingApplyingSchool.SchoolFaithSchool.ConvertDynamicsIntBool(),
+                SchoolFaithSchoolDioceseName = stagingApplyingSchool.SchoolFaithSchoolDioceseName,
+                SchoolFinancialInvestigations = stagingApplyingSchool.SchoolFinancialInvestigations.ConvertDynamicsIntBool(),
+                SchoolFinancialInvestigationsExplain = stagingApplyingSchool.SchoolFinancialInvestigationsExplain,
+                SchoolFinancialInvestigationsTrustAware = stagingApplyingSchool.SchoolFinancialInvestigationsTrustAware.ConvertDynamicsIntBool(),
+                SchoolFurtherInformation = stagingApplyingSchool.SchoolFurtherInformation,
+                SchoolLaClosurePlans = stagingApplyingSchool.SchoolLaClosurePlans.ConvertDynamicsIntBool(),
+                SchoolLaClosurePlansExplain = stagingApplyingSchool.SchoolLaClosurePlansExplain,
+                SchoolLaReorganization = stagingApplyingSchool.SchoolLaReorganization.ConvertDynamicsIntBool(),
+                SchoolLaReorganizationExplain = stagingApplyingSchool.SchoolLaReorganizationExplain,
+                SchoolPFYCapitalForward = stagingApplyingSchool.SchoolPFYCapitalForward,
+                SchoolPFYCapitalForwardStatusExplained = stagingApplyingSchool.SchoolPFYCapitalForwardStatusExplained,
+
+                SchoolPFYCapitalIsDeficit = stagingApplyingSchool.SchoolPFYCapitalSurplusOrDeficit.ConvertSurplusOrDeficit(),
+                SchoolPFYEndDate = stagingApplyingSchool.SchoolPFYEndDate,
+                SchoolPFYRevenue = stagingApplyingSchool.SchoolPFYRevenue,
+                SchoolPFYRevenueIsDeficit = stagingApplyingSchool.SchoolPFYRevenueSurplusOrDeficit.ConvertSurplusOrDeficit(),
+                SchoolPFYRevenueStatusExplained = stagingApplyingSchool.SchoolPFYRevenueStatusExplained,
+
+                SchoolCFYCapitalForward = stagingApplyingSchool.SchoolCFYCapitalForward,
+                SchoolCFYCapitalForwardStatusExplained = stagingApplyingSchool.SchoolCFYCapitalForwardStatusExplained,
+                SchoolCFYCapitalIsDeficit = stagingApplyingSchool.SchoolCFYCapitalSurplusOrDeficit.ConvertSurplusOrDeficit(),
+                SchoolCFYEndDate = stagingApplyingSchool.SchoolCFYEndDate,
+                SchoolCFYRevenue = stagingApplyingSchool.SchoolCFYRevenue,
+                SchoolCFYRevenueIsDeficit = stagingApplyingSchool.SchoolCFYRevenueSurplusOrDeficit.ConvertSurplusOrDeficit(),
+                SchoolCFYRevenueStatusExplained = stagingApplyingSchool.SchoolCFYRevenueStatusExplained,
+
+                SchoolNFYCapitalForward = stagingApplyingSchool.SchoolNFYCapitalForward,
+                SchoolNFYCapitalForwardStatusExplained = stagingApplyingSchool.SchoolNFYCapitalForwardStatusExplained,
+                SchoolNFYCapitalIsDeficit = stagingApplyingSchool.SchoolNFYCapitalSurplusOrDeficit.ConvertSurplusOrDeficit(),
+                SchoolNFYEndDate = stagingApplyingSchool.SchoolNFYEndDate,
+                SchoolNFYRevenue = stagingApplyingSchool.SchoolNFYRevenue,
+                SchoolNFYRevenueIsDeficit = stagingApplyingSchool.SchoolNFYRevenueSurplusOrDeficit.ConvertSurplusOrDeficit(),
+                SchoolNFYRevenueStatusExplained = stagingApplyingSchool.SchoolNFYRevenueStatusExplained,
+                
+                SchoolPartOfFederation = stagingApplyingSchool.SchoolPartOfFederation.ConvertDynamicsIntBool(),
+                SchoolSACREExemption = stagingApplyingSchool.SchoolSACREExemption.ConvertDynamicsIntBool(),
+                SchoolSACREExemptionEndDate = stagingApplyingSchool.SchoolSACREExemptionEndDate,
+                SchoolSupportedFoundation = stagingApplyingSchool.SchoolSupportedFoundation.ConvertDynamicsIntBool(),
+                SchoolSupportedFoundationBodyName = stagingApplyingSchool.SchoolSupportedFoundationBodyName,
+                SchoolSupportGrantFundsPaidTo = stagingApplyingSchool.SchoolSupportGrantFundsPaidTo.ConvertFundsPaidTo(),
+
+                SchoolLeases = new List<A2BSchoolLease>(),
+                SchoolLoans = new List<A2BSchoolLoan>()
+            }
         };
 
         ApplyingSchoolMapper mapper = new(_mockSchoolLease.Object, _mockSchoolLoan.Object);
