0b012b107eb86c5cf1ae602602e2da5f80f5982c
MartinWheelerMT <martin.wheeler@madetech.com>
2022-05-04 15:47:40 +0100

    Remove unneeded properties from AcademyConversionProject model Remove unnecessary usings

--- ProcessApplicationFormFunction/Mappers/ProjectMapper.cs
+++ ProcessApplicationFormFunction/Mappers/ProjectMapper.cs
@@ -2,7 +2,6 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 using System.Linq;
-using System.Net.Mime;
 using ProcessApplicationFormFunction.Database.Models;
 using ProcessApplicationFormFunction.Extensions;
 
